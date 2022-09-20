12940656d47a5a0a59b8ba09c316130fa55f7779
cut0 <zeron9421@gmail.com>
2022-09-20 18:05:34 +0900

    :sparkles: vanilla-extract の設定

--- next.config.js
+++ next.config.js
@@ -3,10 +3,13 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = withBundleAnalyzer(withPreact(nextConfig));
+module.exports = withBundleAnalyzer(withVanillaExtract(withPreact(nextConfig)));
