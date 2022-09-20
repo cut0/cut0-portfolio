ece1055f78ef56436f1c97bd841686fa1b0df039
cut0 <zeron9421@gmail.com>
2022-09-20 21:31:48 +0900

    :sparkles: 経歴追加

---
+++ src/features/career/Content.ts
@@ -0,0 +1,68 @@
+type CareerContentType = {
+  year: string;
+  summary: string;
+}[];
+
+export const CareerContentList: CareerContentType = [
+  {
+    year: " ~2019年",
+    summary:
+      "数理研究部でUnityを用いたVRコンテンツの開発やXamarin.Androidを用いたAndroidアプリケーションの開発",
+  },
+  {
+    year: "2019年4月",
+    summary: "東京理科大学工学部情報工学科に入学",
+  },
+  {
+    year: "2019年5月 ~ 11月",
+    summary: "ベンチャー企業でインターン",
+  },
+  {
+    year: "2019年10月",
+    summary: "プログラミングサークルairouに参加",
+  },
+  {
+    year: "2019年12月 ~ 2021年1月",
+    summary: "株式会社フューチャーリンクネットワークで開発",
+  },
+  {
+    year: "2020年10月",
+    summary: "アフターコロナハッカソン参加",
+  },
+  {
+    year: "2021年2月 ~ 6月",
+    summary: "株式会社ミクシィで開発",
+  },
+  {
+    year: "2021年7月",
+    summary: "ピクシブ株式会社で開発",
+  },
+  {
+    year: "2021年8月",
+    summary: "株式会社サイバーエージェントで開発",
+  },
+  {
+    year: "2021年9月",
+    summary: "合同会社DMM.comで開発",
+  },
+  {
+    year: "2021年10月 ~ 2022年3月",
+    summary: "株式会社ミクシィで開発",
+  },
+  {
+    year: "2021年4月 ~ 2022年6月",
+    summary: "株式会社エウレカで開発",
+  },
+  {
+    year: "2022年7月",
+    summary: "株式会社ゆめみの 3days インターン参加",
+  },
+  {
+    year: "2022年8月",
+    summary: "社内 Web Speed Hackathon で1位を逃す",
+  },
+  {
+    year: "2022年9月",
+    summary: "Open Hack U で入賞を逃す",
+  },
+];
