8be954ffa036aacdef8639d057df8489bdc6f8fe
cut0 <zeron9421@gmail.com>
2022-09-20 21:31:32 +0900

    :sparkles: theme 設定

---
+++ src/features/styles/global.css.ts
@@ -0,0 +1,35 @@
+import { globalStyle } from "@vanilla-extract/css";
+import { background, fontSize, text } from "./theme";
+
+globalStyle("html", {
+  fontSize: "16px",
+  textSizeAdjust: "100%",
+  color: text.default,
+  backgroundColor: background.default,
+});
+
+globalStyle("a", {
+  textDecoration: "none",
+});
+
+globalStyle("li", {
+  listStyleType: "none",
+});
+
+globalStyle("p", {
+  fontSize: fontSize.s7,
+  "@media": {
+    "screen and (min-width: 768px)": {
+      fontSize: fontSize.s6,
+    },
+  },
+});
+
+globalStyle("span", {
+  fontSize: fontSize.s7,
+  "@media": {
+    "screen and (min-width: 768px)": {
+      fontSize: fontSize.s6,
+    },
+  },
+});
---
+++ src/features/styles/theme.ts
@@ -0,0 +1,34 @@
+export const color = {
+  minsk: "#49337D",
+  steelGray: "#2C2738",
+  tanHide: "#F68C4E",
+  white: "#FFFFFF",
+  carnation: "#FF515B",
+  springGreen: "#23FF95",
+} as const;
+
+export const basic = {
+  primary: color.minsk,
+  error: color.carnation,
+  success: color.springGreen,
+} as const;
+
+export const background = {
+  default: color.steelGray,
+  light: color.minsk,
+} as const;
+
+export const text = {
+  default: color.white,
+  notice: color.tanHide,
+} as const;
+
+export const fontSize = {
+  s1: "2.5rem",
+  s2: "2rem",
+  s3: "1.75rem",
+  s4: "1.5rem",
+  s5: "1.25rem",
+  s6: "1rem",
+  s7: ".875rem",
+} as const;
